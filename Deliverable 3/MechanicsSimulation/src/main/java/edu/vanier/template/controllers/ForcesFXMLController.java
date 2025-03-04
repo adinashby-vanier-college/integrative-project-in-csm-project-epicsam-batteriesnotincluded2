b81d15b487b6ebc796bbe625e89eaa97ff0e10f3
Darius Bruno <6227309@edu.vaniercollege.qc.ca>
2025-03-04 10:43:10 -0500

    added more ui elements to forces simulation

--- Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/ForcesFXMLController.java
+++ Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/ForcesFXMLController.java
@@ -7,8 +7,8 @@ package edu.vanier.template.controllers;
 import edu.vanier.template.ui.MainApp;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
 
 /**
  *
@@ -19,12 +19,23 @@ public class ForcesFXMLController {
     Button btnBack;
     
     @FXML
+    
+    MenuBar menuBar;
+    
+    @FXML
+    Menu menuHelp;
+    
+    @FXML
     MenuItem mitBack;
+    
+    @FXML
+    PieChart pieForceDistribution;
     
     @FXML
     public void initialize() {
         mitBack.setOnAction(this::loadPrimaryScene);
-        btnBack.setOnAction(Event -> {btnBack.setText("GO TO HELP!");});
+        btnBack.setOnAction(Event -> {btnBack.setText("GO TO HELP!");
+        });
     }
 
     private void loadPrimaryScene(Event e) {
