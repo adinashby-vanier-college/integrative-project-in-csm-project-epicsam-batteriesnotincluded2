f164b991fc20e58f74bea73eea8b5ac6c9aecb99
Andy123451 <158304574+Andy123451@users.noreply.github.com>
2025-03-05 22:11:41 -0500

    Added menu bar to momentum sim

--- Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/MomentumFXMLController.java
+++ Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/MomentumFXMLController.java
@@ -7,21 +7,56 @@ package edu.vanier.template.controllers;
 import edu.vanier.template.ui.MainApp;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 /**
  *
  * @author theli
  */
 public class MomentumFXMLController {
+
     @FXML
-    Button btnBack;
+    MenuBar menuBar;
+    
+    @FXML
+    Menu menuFile, menuEdit, menuHelp;
+    
+    @FXML
+    MenuItem mitBack;    
+        
+    @FXML
+    MenuItem mitAbout, mitTips;
     
     @FXML
     public void initialize() {
-        btnBack.setOnAction(this::loadPrimaryScene);
+        initMenu();
+    }
+    
+    private void initMenu(){//initializes the buttons in the menu bar
+        //File
+        mitBack.setOnAction(this::loadPrimaryScene);
+        //Edit
+        //Help
+        mitAbout.setOnAction((event)->{
+        Alert("Momentum simulation","s","s");
+        });
+        mitTips.setOnAction((event)->{
+        Alert("Some tips","Here's my tips","That's right");
+        });
     }
 
+    private static void Alert(String title, String header, String content){
+       Alert alert = new Alert(Alert.AlertType.INFORMATION);
+       alert.setTitle(title);
+       alert.setHeaderText(header);
+       alert.setContentText(content);
+       alert.show();
+    }
+    
     private void loadPrimaryScene(Event e) {
         MainApp.switchScene(MainApp.MAINAPP_SCENE);
     }
