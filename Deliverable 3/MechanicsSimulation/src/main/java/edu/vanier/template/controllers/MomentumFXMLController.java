47d3268985cbeb4ac3c30a5e57e86586f6ef2e5b
Andy123451 <158304574+Andy123451@users.noreply.github.com>
2025-03-16 20:00:56 -0400

    Momentum UI complete
    All UIs in momentum sim are in place with the important ones haveing FXIDs. The balls can also now be dragged with the mouse.

--- Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/MomentumFXMLController.java
+++ Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/MomentumFXMLController.java
@@ -7,17 +7,22 @@ package edu.vanier.template.controllers;
 import edu.vanier.physics.Momentum;
 import edu.vanier.template.ui.MainApp;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -57,6 +62,15 @@ public class MomentumFXMLController {
     @FXML
     Pane momRoot;//the momentum root pane where the animation is taking place
             
+    @FXML
+    Label lbX1, lbX2, lbX3, lbX4, lbX5, lbY1, lbY2, lbY3, lbY4, lbY5;//to display the positions of all balls     
+    
+    @FXML
+    Slider slM1;//sliders for the mass of balls 
+            
+    @FXML
+    Label lbOne, lbTwo, lbThree, lbFour, lbFive;//the labels on top of the balls so it's easier to tell which ball's number what       
+            
     Stage stg;
     
     SpinnerValueFactory <Integer> balls;
@@ -69,6 +83,11 @@ public class MomentumFXMLController {
     
     Momentum b1, b2, b3, b4, b5;//ball objects that correspond to every circle object
     
+    double orgSceneX, orgSceneY;//original mouse click positions
+    double ogBallX, ogBallY;//original position of the dragged ball
+    
+    int data = 1;//determines which ball's data to display
+    
     public void setStage(Stage stage){
     this.stg = stage;
     }
@@ -78,6 +97,7 @@ public class MomentumFXMLController {
         initMenu();
         setUpSpinners();
         setUpButtons();
+        setUpSliders();
         generateBalls();
         initSetup();
     }
@@ -88,21 +108,55 @@ public class MomentumFXMLController {
         btnB3.setDisable(true);
         btnB4.setDisable(true);
         btnB5.setDisable(true);
+        
+        lbOne.toFront();
+        lbTwo.setVisible(false);
+        lbTwo.toFront();
+        lbThree.setVisible(false);
+        lbThree.toFront();
+        lbFour.setVisible(false);
+        lbFour.toFront();
+        lbFive.setVisible(false);
+        lbFive.toFront();
+        
+        setBallLabels();//set the ball labels' positions to be right ontop of the balls
     }
+    
+    
     
     private void generateBalls(){
         b1 = new Momentum(30,30,2,3);//x, y, mass, velocity
-        b2 = new Momentum(80,80,2,3);
+        b2 = new Momentum(400,400,2,3);
         b3 = new Momentum(130,130,2,3);
         b4 = new Momentum(180,180,2,3);
         b5 = new Momentum(230,230,2,3);
         
-        c1 = new Circle(b1.getPositionX(),b1.getPositionY(),20);
-        c2 = new Circle(b2.getPositionX(),b2.getPositionY(),20);
-        c3 = new Circle(b3.getPositionX(),b3.getPositionY(),20);
-        c4 = new Circle(b4.getPositionX(),b4.getPositionY(),20);
-        c5 = new Circle(b5.getPositionX(),b5.getPositionY(),20);
+        c1 = new Circle(b1.getPositionX(),b1.getPositionY(),20, Color.RED);
+        c1.setStroke(Color.BLACK);
+        c2 = new Circle(b2.getPositionX(),b2.getPositionY(),20, Color.GREEN);
+        c2.setStroke(Color.BLACK);
+        c3 = new Circle(b3.getPositionX(),b3.getPositionY(),20, Color.BLUE);
+        c3.setStroke(Color.BLACK);
+        c4 = new Circle(b4.getPositionX(),b4.getPositionY(),20, Color.BROWN);
+        c4.setStroke(Color.BLACK);
+        c5 = new Circle(b5.getPositionX(),b5.getPositionY(),20, Color.PURPLE);
+        c5.setStroke(Color.BLACK);
 
+        c1.setOnMouseDragged(circleOnMousePressedEventHandler(c1));
+        c1.setOnMouseDragged(circleOnMouseDraggedEventHandler(c1));
+        
+        c2.setOnMouseDragged(circleOnMousePressedEventHandler(c2));
+        c2.setOnMouseDragged(circleOnMouseDraggedEventHandler(c2));
+        
+        c3.setOnMouseDragged(circleOnMousePressedEventHandler(c3));
+        c3.setOnMouseDragged(circleOnMouseDraggedEventHandler(c3));
+        
+        c4.setOnMouseDragged(circleOnMousePressedEventHandler(c4));
+        c4.setOnMouseDragged(circleOnMouseDraggedEventHandler(c4)); 
+        
+        c5.setOnMouseDragged(circleOnMousePressedEventHandler(c5));
+        c5.setOnMouseDragged(circleOnMouseDraggedEventHandler(c5));
+    
         momRoot.getChildren().add(c1);
         momRoot.getChildren().add(c2);
         momRoot.getChildren().add(c3);
@@ -115,7 +169,44 @@ public class MomentumFXMLController {
         c5.setVisible(false);
 
     }
+
     
+    private EventHandler<MouseEvent> circleOnMousePressedEventHandler(Circle ball){
+    return new EventHandler<MouseEvent>(){
+    @Override
+        public void handle(MouseEvent t) {
+            orgSceneX = t.getSceneX();//mouse click x position
+            orgSceneY = t.getSceneY();//mouse click y position
+            ogBallX = ball.getCenterX();//records ball's starting position
+            ogBallY = ball.getCenterY();
+        }
+    };
+    }
+    
+    private EventHandler<MouseEvent> circleOnMouseDraggedEventHandler(Circle ball){    
+        return new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent t) {
+            double offsetX = t.getSceneX() - orgSceneX;//how much the mouse dragged away the ball
+            double offsetY = t.getSceneY() - orgSceneY;
+
+            ball.setCenterX(ogBallX + offsetX - 20);//original position + mouse dragged diistance + correction factor
+            ball.setCenterY(ogBallY + offsetY - 40);
+            
+            switch(data){
+                case 1: showValues(1);break;
+                case 2: showValues(2);break;
+                case 3: showValues(3);break;
+                case 4: showValues(4);break;
+                case 5: showValues(5);break;
+                default: System.out.println("Something's wrong"); break;
+            }
+            
+            setBallLabels();         
+        }
+    };
+    }
+
     private void initMenu(){//initializes the buttons in the menu bar
         //File
         mitBack.setOnAction(this::loadPrimaryScene);
@@ -135,12 +226,12 @@ public class MomentumFXMLController {
     private void setUpSpinners(){
        balls = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5, 1, 1);//min 1 ball, max 5 balls, adds 1 at a time
        spBalls.setValueFactory(balls);
-       spBalls.getEditor().setStyle("-fx-font-size: 20px");
        
        spBalls.valueProperty().addListener((event)->{
            switch (balls.getValue()) {
                case 1: 
                    setUIsVisible(false, false, false, false);
+                   showValues(1);
                    break;
                case 2:
                    setUIsVisible(true, false, false, false);
@@ -164,10 +255,16 @@ public class MomentumFXMLController {
     //to disable/enable UIs with flags based on how many balls there are
     //c1 will always be visible
     private void setUIsVisible(boolean f2, boolean f3, boolean f4, boolean f5){
+        
         c2.setVisible(f2);
         c3.setVisible(f3);
         c4.setVisible(f4);
         c5.setVisible(f5);
+        
+        lbTwo.setVisible(f2);
+        lbThree.setVisible(f3);
+        lbFour.setVisible(f4);
+        lbFive.setVisible(f5);
         
         //the reverse of the inputted booleans for functions with effects reversive of "setVisible"
         boolean rev2, rev3, rev4, rev5;
@@ -191,6 +288,7 @@ public class MomentumFXMLController {
     }
     
     private void setUpButtons() {
+       
        btnPlayPause.setOnAction((event)->{
           if(PlayPause.equals("paused")){
              ivPlayPause.setImage(new Image("/Images/pause.png"));
@@ -201,6 +299,48 @@ public class MomentumFXMLController {
              PlayPause = "paused";//now the animation is paused
           }
        });
+       
+       btnB1.setOnAction((event)->{data = 1; showValues(1);});//ball 1's data will now be displayed
+       btnB2.setOnAction((event)->{data = 2; showValues(2);});//ball 2's data will now be displayed
+       btnB3.setOnAction((event)->{data = 3; showValues(3);});//ball 3's data will now be displayed
+       btnB4.setOnAction((event)->{data = 4; showValues(4);});//ball 4's data will now be displayed
+       btnB5.setOnAction((event)->{data = 5; showValues(5);});//ball 5's data will now be displayed
+
+    }
+    
+    private void setBallLabels(){
+            lbOne.setLayoutX(c1.getCenterX()-5);
+            lbOne.setLayoutY(c1.getCenterY()-13);
+            lbTwo.setLayoutX(c2.getCenterX()-5);
+            lbTwo.setLayoutY(c2.getCenterY()-13);
+            lbThree.setLayoutX(c3.getCenterX()-5);
+            lbThree.setLayoutY(c3.getCenterY()-13);
+            lbFour.setLayoutX(c4.getCenterX()-5);
+            lbFour.setLayoutY(c4.getCenterY()-13);
+            lbFive.setLayoutX(c5.getCenterX()-5);
+            lbFive.setLayoutY(c5.getCenterY()-13);
+    }
+    
+    private void setUpSliders() {
+       slM1.valueProperty().addListener((event)->{          
+
+       });
+    }
+    
+    private void showValues(int i){
+       
+        Circle c = c1;
+        switch(i){
+            case 1: c = c1; break;     
+            case 2: c = c2; break;
+            case 3: c = c3; break;
+            case 4: c = c4; break;
+            case 5: c = c5; break;
+            default: System.out.println("Error");
+        }
+        
+        //lbPositionX.setText(String.valueOf(c.getCenterX() + "x"));
+        //lbPositionY.setText(String.valueOf(c.getCenterY() + "y"));       
     }
     
     private static void Alert(String title, String header, String content){
@@ -214,5 +354,7 @@ public class MomentumFXMLController {
     private void loadPrimaryScene(Event e) {
         MainApp.switchScene(MainApp.priStage, MainApp.MAINAPP_SCENE);
     }
+
+    
 
 }
