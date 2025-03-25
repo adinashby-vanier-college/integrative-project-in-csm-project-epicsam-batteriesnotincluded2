f8fbf51d6b660f2a98faa66edfd203c1ae98eb7e
Darius Bruno <6227309@edu.vaniercollege.qc.ca>
2025-03-25 11:37:03 -0400

    Forces arrows added
    you can now spawn arrows and drag them around the box. working on selecting them and the selected vector box.

--- Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/ForcesFXMLController.java
+++ Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/controllers/ForcesFXMLController.java
@@ -4,22 +4,26 @@
  */
 package edu.vanier.template.controllers;
 
-import edu.vanier.template.ui.MainApp;
-import javafx.event.Event;
-import javafx.fxml.FXML;
+import edu.vanier.template.ui.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
 
 /**
- *
+ * The class that controls the FXML elements of the Forces simulation
  * @author ChiliWasp
  */
 public class ForcesFXMLController {
-    @FXML
-    Button btnBack;
     
     @FXML
+    Button btnBack, btnIgnore, btnDelete, btnSummonVector;
     
+    @FXML
     MenuBar menuBar;
     
     @FXML
@@ -29,15 +33,120 @@ public class ForcesFXMLController {
     MenuItem mitBack;
     
     @FXML
+    Slider slMag,slDir;
+    
+    @FXML
     PieChart pieForceDistribution;
     
     @FXML
+    Pane megaPane;
+    
+    @FXML
+    StackPane Arrow;
+    
+    @FXML
+    Rectangle box,recBackground;
+    
+    int i=1;
+    boolean selectingVector=false;
+    
+    @FXML
     public void initialize() {
-        mitBack.setOnAction(this::loadPrimaryScene);
         btnBack.setVisible(false);
-        btnBack.setOnAction(Event -> {btnBack.setText("GO TO HELP!");
+        
+        mitBack.setOnAction(this::loadPrimaryScene);
+        btnSummonVector.setOnAction((event)->{
+        VectorArrow sp=new VectorArrow(btnSummonVector.getLayoutX(), btnSummonVector.getLayoutY());
+        sp.setVisible(true);
+        sp.setOnMouseDragged(onVectorDragged(sp));
+        sp.setOnMousePressed(onVectorPressed(sp));
+        
+         megaPane.getChildren().add(sp);
         });
+        int x=2;
+        while (x>0){
+            x=(int)Math.rint(Math.random()*16);
+            System.out.println(x);
+        }
+        
+        slMag.valueProperty().addListener((event)->{
+        
+        });
+        btnDelete.setOnAction((event)->{
+            for (Node n:megaPane.getChildren()){
+                if (n instanceof VectorArrow){
+                    if (((VectorArrow) n).getSelected()){
+                        megaPane.getChildren().remove(n); break;}
+                }
+            }
+        });
+        
+        for (Node n:megaPane.getChildren()) n.setOnMousePressed(onVectorPressed(n));
     }
+    private void toggleSelectedVector(){
+        if(selectingVector){
+        slMag.setDisable(false);
+        slDir.setDisable(false);
+        btnIgnore.setDisable(false);
+        btnDelete.setDisable(false);
+            
+        }
+        else {
+        slMag.setDisable(true);
+        slDir.setDisable(true);
+        btnIgnore.setDisable(true);
+        btnDelete.setDisable(true);
+        }
+    }
+    private void unselectAllVectors(){
+        for (Node v:megaPane.getChildren()) if (v instanceof VectorArrow) ((VectorArrow)v).setSelected(false);
+        selectingVector=false;
+    }
+    
+    /**
+     * Handles clicking of a vector arrow.
+     * @param v The vector arrow clicked
+     * @return the stats clicked, bro.
+     */
+    private EventHandler<MouseEvent> onVectorPressed(Node n){
+        return (MouseEvent t) -> {
+            if (n instanceof VectorArrow){
+                unselectAllVectors();
+                ((VectorArrow) n).setSelected(true); 
+                selectingVector=true;
+                toggleSelectedVector();
+            } else {
+                unselectAllVectors();
+                toggleSelectedVector();
+            }
+        };
+    };
+    /**
+     * Moves the selected vector to the mouse (you can drag the vector around)
+     * @param v The vector arrow being dragged
+     * @return the new position of the vector arrow
+     */
+    private EventHandler<MouseEvent> onVectorDragged(VectorArrow v){
+        return (MouseEvent t) -> {
+            v.setLayoutX(t.getSceneX()/*-(186/2)*/);
+            v.setLayoutY(t.getSceneY()-(88/2));
+            double ArrowX=v.getBoundsInParent().getCenterX();
+            double ArrowY=v.getBoundsInParent().getCenterY();
+            double boxCenterX=box.getBoundsInParent().getCenterX();
+            double boxCenterY=box.getBoundsInParent().getCenterY();
+//            System.out.println(ArrowX);
+//            System.out.println(boxCenterX);
+//            System.out.println(ArrowY);
+//            System.out.println(boxCenterY);
+            double rotate=Math.atan((boxCenterY-ArrowY)/(boxCenterX-ArrowX));
+//            System.out.println(rotate);
+            rotate=rotate/Math.PI*180;
+            if (ArrowX<=boxCenterX)
+            v.setRotate(rotate);
+            else v.setRotate(rotate +180);
+        };
+    }
+    
 
     private void loadPrimaryScene(Event e) {
         MainApp.switchScene(MainApp.priStage, MainApp.MAINAPP_SCENE);
---
+++ Deliverable 3/MechanicsSimulation/src/main/java/edu/vanier/template/ui/VectorArrow.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.template.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.*;
+
+/**
+ *
+ * @author ChiliWasp
+ */
+public class VectorArrow extends StackPane{
+    boolean selected=false;
+
+    public boolean getSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    
+    
+    public VectorArrow(double x, double y){
+        VectorArrow.this.setWidth(186);
+        VectorArrow.this.setHeight(88);
+        
+        VectorArrow.this.setLayoutX(x);
+        VectorArrow.this.setLayoutY(y);
+        
+        VectorArrow.this.setAlignment(Pos.CENTER_RIGHT);
+        Rectangle stem=new Rectangle(10, 0, 111, 26);
+        stem.setTranslateX(-62);
+        Polygon tip=new Polygon(0,28,66,-16,0,-60);
+        tip.setScaleX(0.9);
+        tip.setScaleY(0.7);
+        String color="";
+        String point0=".0";
+        
+        for (int i=0;i<6;i++){
+            String hxdml=Double.toString((int)Math.rint(Math.random()*16));
+            hxdml=hxdml.replaceAll(point0, "");
+            switch (hxdml){
+                case "10": hxdml="a"; break;
+                case "11": hxdml="b"; break;
+                case "12": hxdml="c"; break;
+                case "13": hxdml="d"; break;
+                case "14": hxdml="e"; break;
+                case "15": hxdml="f"; break;
+                case "16": hxdml="a"; break;
+                default: break;
+            }
+            color=color+hxdml;
+        }
+        color=color.replaceAll(point0, "");
+//        while (color.contains(point0)){
+//            if (color.substring(index, index+2).equals(point0))
+//                color=color.substring(index-0,index)+color.substring(index+2);
+//            
+//            else index++;
+//        }
+        while (color.length()<6){
+            color=color+"0";
+        }
+        color=color.trim();
+        System.out.println(color);
+        stem.setFill(Paint.valueOf(color));
+        tip.setFill(Paint.valueOf(color));
+        
+        VectorArrow.this.getChildren().addAll(stem,tip);
+    }
+}
